d3f3fbd0988b87f5eeec958bd89b041f065a3b0d
yorman.carrillo <yorman.carrillo@uptc.edu.co>
2024-12-09 02:21:20 +0000

    Se agrego la pagina de reportes con datos simulados

--- src/pages/ReportsPage.jsx
+++ src/pages/ReportsPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Reports } from '../components/reports/Reports'
 
 export const ReportsPage = () => {
   return (
-    <div>ReportsPage</div>
+    <Reports/>
   )
 }
